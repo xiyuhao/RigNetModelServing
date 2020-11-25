e0f945df31b2f2966fc380a0cd1fbd46bc81ea72
Sandra Lopez <slopezza@outlook.com>
2020-11-25 13:08:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'PersonalWebsite'
-    MODEL_FOLDER = 'model\code'
+    MODEL_FOLDER = 'model/code'
     UPLOAD_FOLDER = os.path.join(basedir, 'quick_start')# 'quick_start'
